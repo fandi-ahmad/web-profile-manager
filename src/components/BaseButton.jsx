c08b02181f214e8a702b3d307480238309fd8c9b
fandi-ahmad <fandi4160@gmail.com>
2023-07-04 12:51:35 +0800

    update button component

--- src/components/BaseButton.jsx
+++ src/components/BaseButton.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export const BasicButton = (props) => {
     return (
         <button onClick={props.onClick} className={`button ${props.className || 'blue'}`} id={props.id} type={props.type}>
+            <span>{props.title || 'title here'}</span>
             <span className={`icon ${props.iconShow || 'hidden'}`}>
                 <i className={`mdi ${props.icon || 'mdi-credit-card-outline'}`}></i>
             </span>
-            <span>{props.title || 'title here'}</span>
         </button>
     )
 }
